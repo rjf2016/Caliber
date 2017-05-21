0d0ca1b81e537551714115bd76d6f48d3704c9fd
tjf081 <rickyfahey@hotmail.com>
2017-05-21 16:55:26 -0400

    added additional email field functionality for sender address

--- sendmail2.php
+++ sendmail2.php
@@ -8,10 +8,11 @@ require 'vendor/autoload.php';
 // require("path/to/sendgrid-php/sendgrid-php.php");
 $client_email = $_POST['email_id'];
 
-$from = new SendGrid\Email("Caliber User", "tjf081@gmail.com");
+//$from = new SendGrid\Email("Caliber User", "tjf081@gmail.com");
+$from = new SendGrid\Email("Caliber User", $client_email);
 $subject = "Email from Caliber Partners site";
 $to = new SendGrid\Email("New User", "tjf081@gmail.com");
-$content = new SendGrid\Content("text/plain", "New email from: " + $client_email);
+$content = new SendGrid\Content("text/plain", "New email from: " . ' ' . $client_email);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
